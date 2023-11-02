e48813562a324879dc7c0bf29de50caba2c8cf5d
Zonom <veiror300@gmail.com>
2023-11-03 00:58:31 +1200

    Авторизация и аутентификация пройдено

---
+++ app/exceptions.py
@@ -0,0 +1,34 @@
+from fastapi import HTTPException, status
+
+class BookingException(HTTPException):
+    status_code = 500
+    detail = ""
+    
+    def __init__(self):
+        super().__init__(status_code=self.status_code, detail=self.detail)
+
+
+class UserAlreadyExistsException(BookingException):
+    status_code=status.HTTP_409_CONFLICT
+    detail='Пользователь уже существует'
+
+
+class IncorrectEmailOrPasswordException(BookingException):
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail= 'Неверная почта или пароль',
+
+class TokenExpiredException(BookingException):
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail='Токен истек',
+
+class TokenAbsentException(BookingException):
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail='Токен отсутствует',
+
+class IncorrectTokenFormatException(BookingException):
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail='Неверный формат токена',
+
+class UserIsNotInTokenException(BookingException):
+    status_code=status.HTTP_401_UNAUTHORIZED
+
--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 from jose import jwt, JWTError
+from app.exceptions import IncorrectTokenFormatException, TokenAbsentException,TokenExpiredException, UserIsNotInTokenException
 from app.users.dao import UsersDAO
 from config import settings
-from fastapi import Depends, HTTPException, Request, status
+from fastapi import Depends, Request
 
 
 def get_token(request: Request):
-    token = request.cookies.get('booking_access_token')
+    token = request.cookies.get('booking_access_token') # проверяем есть ли токен
     if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    return token
+        raise TokenAbsentException
+    return tokenо
 
 async def get_current_user(token:str = Depends(get_token)):
 
@@ -17,19 +18,18 @@ async def get_current_user(token:str = Depends(get_token)):
         payload = jwt.decode(
             token, settings.SECRET_KEY, settings.ALGORITHM
         )
-    except:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=1)
-    
+    except JWTError:
+        raise IncorrectTokenFormatException
+
     expire: str = payload.get("exp") #проверяем закончилось ли время жизни токена
     if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=2)
+        raise TokenExpiredException
 
     user_id: str = payload.get("sub") # получаем id пользователя из токена
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    
+        raise UserIsNotInTokenException
     user = await UsersDAO.find_one_or_none(id = int(user_id)) # проверяем есть ли пользователь с id = user_id в БД
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotInTokenException
 
     return user
